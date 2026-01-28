a16dcdd429f5c1c082b3011bf4b60b3880e610b4
mahatosoftware <mahatosoftware@gmail.com>
2026-01-28 07:58:54 +0530

    Fix Button Label

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "in.mahato.tambola"
         minSdk = 23
         targetSdk = 36
-        versionCode = 31
-        versionName = "1.0.31"
+        versionCode = 32
+        versionName = "1.0.32"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
