139dade6dc89d6865ea15675a3eb7f58455e0e3f
mahatosoftware <mahatosoftware@gmail.com>
2026-02-26 01:03:23 +0530

    remove ad key

--- android/app/src/main/java/in/mahato/tambola/util/AdConfig.kt
+++ android/app/src/main/java/in/mahato/tambola/util/AdConfig.kt
@@ -5,10 +5,5 @@ object AdConfig {
     // Application ID: ca-app-pub-8382655413286804~6423628765
 
 
-    // Banner Ad Unit IDs
-    const val BANNER_AD_UNIT_ID = "ca-app-pub-8382655413286804/2266125437"
-
-    // Interstitial Ad Unit IDs
-
-    const val INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-8382655413286804/7364823698"
+   
 }
