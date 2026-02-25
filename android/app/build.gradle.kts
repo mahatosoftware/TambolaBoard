11d07b1f70285d5980e8b37175180c88441aa0c6
mahatosoftware <mahatosoftware@gmail.com>
2026-02-26 01:02:13 +0530

    update version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "in.mahato.tambola"
         minSdk = 23
         targetSdk = 36
-        versionCode = 35
-        versionName = "1.0.35"
+        versionCode = 36
+        versionName = "1.0.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- android/app/src/main/java/in/mahato/tambola/util/AdConfig.kt
+++ android/app/src/main/java/in/mahato/tambola/util/AdConfig.kt
@@ -4,10 +4,11 @@ object AdConfig {
     // AdMob Application ID (for reference, actual value is in AndroidManifest.xml)
     // Application ID: ca-app-pub-8382655413286804~6423628765
 
+
     // Banner Ad Unit IDs
-    const val BANNER_AD_UNIT_ID = ""
+    const val BANNER_AD_UNIT_ID = "ca-app-pub-8382655413286804/2266125437"
 
     // Interstitial Ad Unit IDs
 
-    const val INTERSTITIAL_AD_UNIT_ID = ""
+    const val INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-8382655413286804/7364823698"
 }
