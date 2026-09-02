4b8f2ee0f255f7b8555aac7f53dccd3777caabde
Mahato Software <mahatosoftware@gmail.com>
2026-09-02 21:29:57 +0530

    update version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "in.mahato.tambola"
         minSdk = 23
         targetSdk = 37
-        versionCode = 43
-        versionName = "1.0.43"
+        versionCode = 44
+        versionName = "1.0.44"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
